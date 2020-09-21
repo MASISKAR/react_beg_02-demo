c0f80ed53339c4d0f01ba992fdb26eb573d30339
Masis <masiskar@gmail.com>
2020-09-21 12:06:53 +0400

    understanding modules and git commands

---
+++ src/demo/hello.js
@@ -0,0 +1,24 @@
+
+// export function hello(){
+//     console.log('hello!');
+// }
+
+// export default function hello(){
+//     console.log('hello!');
+// }
+
+function hello(){
+    console.log('hello!');
+}
+
+//  export const name = 'John';
+//  export const surname = 'Smith';
+//  export const age = 15;
+
+const name = 'John';
+const surname = 'Smith';
+const age = 15;
+
+export {name, surname, age};
+
+export default hello;
